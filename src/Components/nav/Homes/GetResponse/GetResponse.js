53bcdd0404d56bee9fd7caf38afb76e19898c0c6
OmerSohail <kousarr054@gmail.com>
2024-07-18 19:46:55 +0500

    c

--- src/Components/nav/Homes/GetResponse/GetResponse.js
+++ src/Components/nav/Homes/GetResponse/GetResponse.js
@@ -34,7 +34,90 @@ export default function GetReponse() {
          
           </div>
 
-          
+         <div className="col-lg-4 col-md-6 col-sl-6 response-main">
+          <div className="res-vision-box">
+            <img className="res-vision-img" src="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1019/LogoECSPublishingGroup1-removebg-preview-1-e1721125158123.png"/>
+            <span className="list-growth ">10% list growth </span><span className="list-growth-black">& sales spike in tough niche</span>
+            <span className="res-vision-p">lt's nice that with GetReponse,we have the tools and integrations we need within our budget. </span>
+            <div className="res-btn-vision"> 
+              Read Case Study
+
+            </div>
+
+          </div>
+          </div> 
+          <div className="col-lg-4 col-md-6 col-sl-6 response-main"  >
+          <div className="res-vision-box">
+            <img className="res-vision-img" src="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1022/living_vision-removebg-preview-e1721125329100.png"/>
+            <span className="list-growth ">75% of all public sales </span><span className="list-growth-black">from email autoresponders</span>
+            <span className="res-vision-p">Together with GetResponse, we've been able to convince constomers of need,culminating in compelling sales. </span>
+            <div className="res-btn-vision"> 
+              Read Case Study
+
+            </div>
+
+          </div>
+          </div> 
+          <div className="col-lg-4 response-main ">
+          <div className="res-vision-box">
+            <img className="res-vision-img" src="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1025/red-hot-logo1_-removebg-preview-e1721125532901.png"/>
+            <span className="list-growth ">Up to $,1000 </span><span className="list-growth-black">for every 1,000 emails sent</span>
+            <span className="res-vision-p">Regarding revenue contribution,the email marketing channel is like free money for us. </span>
+            <div className="res-btn-vision"> 
+              Read Case Study
+            </div>
+          </div>
+          </div>           
+
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
+          <div className="col-lg-2">
+            <div>
+              <img src="https://us-wn-g.gr-cdn.com/_next/static/media/g2_1.6b6f0ef0.png"/>
+
+            </div>
+            
+          </div>
           
           
         </div>
